b8f4524cdbe8f280da9fd87d2a4ac136f630a30f
Riccardo Casatta <riccardo@casatta.it>
2024-10-15 08:07:08 +0200

    timer tick separately for each rpc req

--- src/electrum/server.rs
+++ src/electrum/server.rs
@@ -541,7 +541,6 @@ impl Connection {
         let empty_params = json!([]);
         loop {
             let msg = receiver.recv().chain_err(|| "channel closed")?;
-            let start_time = Instant::now();
             trace!("RPC {:?}", msg);
             match msg {
                 Message::Request(line) => {
@@ -549,12 +548,12 @@ impl Connection {
                     if let Value::Array(arr) = cmd {
                         let mut result = Vec::with_capacity(arr.len());
                         for el in arr {
-                            let reply = self.handle_value(el, &empty_params, start_time)?;
+                            let reply = self.handle_value(el, &empty_params)?;
                             result.push(reply)
                         }
                         self.send_values(&[Value::Array(result)])?
                     } else {
-                        let reply = self.handle_value(cmd, &empty_params, start_time)?;
+                        let reply = self.handle_value(cmd, &empty_params)?;
                         self.send_values(&[reply])?
                     }
                 }
@@ -569,12 +568,8 @@ impl Connection {
         }
     }
 
-    fn handle_value(
-        &mut self,
-        cmd: Value,
-        empty_params: &Value,
-        start_time: Instant,
-    ) -> Result<Value> {
+    fn handle_value(&mut self, cmd: Value, empty_params: &Value) -> Result<Value> {
+        let start_time = Instant::now();
         Ok(
             match (
                 cmd.get("method"),
